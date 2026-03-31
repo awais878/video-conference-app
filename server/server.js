6533d3311930921ed73db3db78e071cfee9e561e
AWAIS NAWAB <awaisnawab@AWAISs-MacBook-Pro.local>
2026-03-31 13:57:51 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("user-joined");
+    });
+
+    socket.on("offer", ({ roomId, offer }) => {
+        socket.to(roomId).emit("offer", offer);
+    });
+
+    socket.on("answer", ({ roomId, answer }) => {
+        socket.to(roomId).emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", ({ roomId, candidate }) => {
+        socket.to(roomId).emit("ice-candidate", candidate);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+const PORT = 5001;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
